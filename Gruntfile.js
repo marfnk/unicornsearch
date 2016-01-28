1993d59b0fa93db5323dacd83e1d365d28f81929
Marius Fink <marius.fink@holisticon.de>
2016-01-28 10:09:08 +0100

    add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+
+		// Empties folders to start fresh
+		clean: ["dist/*.*"],
+
+		compass: {
+			dist: {
+				options: {
+					sassDir: 'src',
+					cssDir: 'dist',
+					//importPath: 'app/bower_components/'
+				}
+			}
+		},
+
+		copy: {
+			dist: {
+                src: 'src/unicornsearch.js',
+                dest: 'dist/unicornsearch.js'
+            }
+		},
+
+		ngtemplates: {
+			app: {
+				cwd: 'src/',
+				src: [
+					'*.html'
+				],
+				dest: 'dist/unicornsearch.js',
+				options: {
+					append: true,
+					module: 'unicornsearchModule',
+					htmlmin: {collapseWhitespace: true}
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('build', function () {
+		return grunt.task.run([
+			'clean',
+			'copy:dist',
+			//'compass:dist',
+			'ngtemplates'
+		]);
+	});
+
+};
--- src/unicornsearch.js
+++ src/unicornsearch.js
@@ -1,13 +1,13 @@
 (function() {
   angular
-  .module('unicornsearchModule', ['ngSanitize']);
-  
+      .module('unicornsearchModule', ['ngSanitize']);
+
   angular
-    .module('unicornsearchModule')
-    .directive('unicornSearch', SearchDirective);
-  
+      .module('unicornsearchModule')
+      .directive('unicornSearch', SearchDirective);
+
   function SearchDirective($q, $timeout, $window) {
-    
+
     return {
       restrict: 'E',
       templateUrl: 'unicornsearch.tpl.html',
@@ -22,6 +22,11 @@
       },
       link: function ($scope, element, attrs, ngModel) {
         var currentTimeout;
+        var searchField;
+        $timeout(function() {
+          searchField = element.find('input')[0];
+        });
+
         $scope.delay = $scope.delay || 400;
         $scope.minChars = $scope.minChars || 3;
         $scope.maxItems = $scope.maxItems;
@@ -32,29 +37,26 @@
         $scope.isAlreadySelected = isAlreadySelected;
         $scope.activateSearchField = activateSearchField;
         $scope.closeResults = closeResults;
+        $scope.openResults = openResults;
         $scope.clear = clear;
         $scope.results = [];
         $scope.setTouched = setTouched;
-        $scope.handleSpecialChars = handleSpecialChars;
+        $scope.handleKeyboardInput = handleKeyboardInput;
         $scope.state = {  open: false,
-                          loading: false,
-                          nothingFound: false, 
-                          loadingError: false,
-                          tooLessLetters: false,
-                          focused: false,
-                          maximumReached: false };
-                          
-        registerClickOutListener();
-         
+          loading: false,
+          nothingFound: false,
+          loadingError: false,
+          tooLessLetters: false,
+          focused: false,
+          maximumReached: false };
+
         $scope.$watch('search', function(searchTerm) {
-          console.log("hallo");
           if ($scope.state.maximumReached) {
             $scope.search = '';
             return;
           }
-          //reset
           resetResults();
-          
+
           if (searchTerm) {
             $scope.state.tooLessLetters = searchTerm.length < $scope.minChars;
             if (searchTerm.length >= $scope.minChars) {
@@ -65,19 +67,19 @@
               currentTimeout = $timeout(function(){
                 startLoading();
                 $scope.load(searchTerm)
-                  .then(addSearchResults, loadingError)
-                  .then(stopLoading);
+                    .then(addSearchResults, loadingError)
+                    .then(stopLoading);
               }, $scope.delay);
             }
           }
         });
-        
+
         $scope.$watchCollection('ngModel.$modelValue', function(n) {
           if (n !== $scope.selectedItems) {
-           $scope.selectedItems = n;
+            $scope.selectedItems = n;
           }
         });
-        
+
         //sets the model to undefined if array is empty
         $scope.$watchCollection('selectedItems', function(n) {
           if (angular.isArray(n) && n.length === 0) {
@@ -86,7 +88,7 @@
             ngModel.$setViewValue($scope.selectedItems);
           }
         });
-        
+
         //observe maximum input
         $scope.$watch('selectedItems.length', function(length) {
           if (typeof $scope.selectedItems === 'undefined') {
@@ -99,109 +101,131 @@
             }
           }
         });
-        
+
         function addSearchResults(results) {
           $scope.results = results;
           $scope.state.nothingFound = results.length === 0;
-          $scope.state.open = true;
+          openResults();
         }
-        
+
         function loadingError(err) {
           $scope.state.loadingError = true;
           stopLoading();
         }
-        
+
         function stopLoading() {
           $scope.state.loading = false;
         }
-        
+
         function startLoading() {
           $scope.state.loading = true;
         }
-        
+
         function selectItem(item) {
           if (!$scope.selectedItems) {
             $scope.selectedItems = [];
           }
           $scope.selectedItems.push(item)
-          $scope.state.open = false;
+          closeResults();
+          activateSearchField();
         }
-        
+
         function removeItem(item) {
           $scope.selectedItems.splice($scope.selectedItems.indexOf(item), 1);
         }
-        
+
         function isAlreadySelected(item) {
           return $scope.selectedItems && $scope.selectedItems.indexOf(item) !== -1;
         }
-        
+
         function activateSearchField() {
           if (!$scope.disabled) {
-            element.find('input')[0].focus();
+            searchField.focus();
+            searchField.select();
           }
         }
-        
+
+        function openResults() {
+          $scope.state.open = true;
+          $window.addEventListener("click", onClickOutside);
+        }
+
         function closeResults() {
           $timeout(function() {
             $scope.state.open = false;
           });
+          $window.removeEventListener("click", onClickOutside);
         }
-        
+
         function resetResults() {
           $scope.results = [];
-          $scope.state.open = false;
+          closeResults();
           $scope.nothingFound = false;
           $scope.loadingError = false;
         }
-        
-        function openResults() {
-          $scope.state.open = true;  
-        }
-        
+
         function clear() {
           $scope.selectedItems = [];
           $scope.results = [];
           $scope.search = '';
           activateSearchField();
         }
-        
+
         function setTouched() {
           ngModel.$setTouched();
         }
-        
-        function inputIsEmpty() {
-          return (!$scope.search || $scope.search.length === 0);
+
+        function onClickOutside(event) {
+          console.log("click outside");
+          if (element[0].contains(event.target)) return;
+          closeResults();
+          $scope.$apply();
         }
-        
-        function registerClickOutListener() {
-          angular.element($window).on('click', function (event) {
-            if (element[0].contains(event.target)) return;
-            closeResults();
-            $scope.$apply();
-          });
+
+        function getFirstSelectableItemFromResults() {
+          for (var i = 0; i < $scope.results.length; i++) {
+            if (!isAlreadySelected($scope.results[i])) {
+              return $scope.results[i];
+            }
+          }
+          return undefined;
         }
-        
-        function handleSpecialChars(event) {
-          var selectables = element[0].querySelectorAll('.unicorn-search__focusable');
-          if (event.key === "ArrowDown" || event.key === "ArrowUp") {
-            $scope.state.open = true;
+
+        function handleKeyboardInput(event) {
+          //keys: http://www.quirksmode.org/js/keys.html
+          var backspace = 8, down = 40, up = 38, esc = 27, enter = 13;
+          var pressed = event.keyCode;
+
+          if (pressed === down || pressed === up) {
+            var selectables = element[0].querySelectorAll('.unicorn-search__focusable');
+            openResults();
             $timeout(function() {
               var index = [].indexOf.call(selectables, event.target);
-              var nextIndex = event.key === "ArrowDown" ? (index+1) : (index-1);
+              var nextIndex = (pressed === down) ? (index+1) : (index-1);
               var nextElement = selectables[Math.min(selectables.length, nextIndex)];
               if (nextElement) {
-                nextElement.focus();  
-              }
+                nextElement.focus();
+              } //else focus first element?
             });
             event.preventDefault();
-          } else if (event.key === "Backspace" && inputIsEmpty()) {
-            $scope.selectedItems.pop();
-            event.preventDefault();
-          } else if (event.key === "Escape") {
+          } else if (pressed === backspace && inputIsEmpty()) {
+            var inputIsEmpty = (!$scope.search || $scope.search.length === 0);
+            if (inputIsEmpty) {
+              $scope.selectedItems.pop();
+              event.preventDefault();
+            }
+          } else if (pressed === esc) {
             activateSearchField();
             closeResults();
+          } else if (pressed === enter && event.currentTarget === searchField) {
+            event.preventDefault();
+            var firstSelectableItem = getFirstSelectableItemFromResults();
+            if ($scope.state.open === true && firstSelectableItem) {
+              selectItem(firstSelectableItem);
+            }
           }
         }
+
       }
     };
   }
